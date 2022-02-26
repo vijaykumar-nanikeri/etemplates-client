91e1c1f23d3deb9ad281636e05b0e364f81a218f
vijaykumar-nanikeri <vijaykumar.nanikeri@gmail.com>
2022-02-26 13:21:17 +0530

    eTemplates: Password pattern.

--- src/app/web/home/shared/components/change-default-password/change-default-password.enum.ts
+++ src/app/web/home/shared/components/change-default-password/change-default-password.enum.ts
@@ -7,14 +7,29 @@ export enum LABELS {
 export enum PASSWORD_FORM_CONTROL_LABELS {
   MinLength = 6,
   MaxLength = 16,
-  Pattern = '^[A-Za-z0-9!#$^*_<>]+$',
+  Pattern = '^[A-Za-z0-9!#$+-@^_~]+$',
   RequiredValidationFeedback = 'Field is required.',
   MinLengthValidationFeedback = 'Minimum length of the field is 6.',
   MaxLengthValidationFeedback = 'Maximum length of the field is 16.',
   PatternValidationFeedback = 'Invalid pattern.',
-  MinLengthInstruction = 'Minimum length of the password is <code>6</code>.',
-  MaxLengthInstruction = 'Maximum length of the password is <code>16</code>.',
-  PatternInstruction = 'Characters Allowed: Case-insensitive, Numbers, Special (<code>!</code>, <code>#</code>, <code>$</code>, <code>^</code>, <code>*</code>, <code>_</code>, <code><</code>, <code>></code> only)',
+  MinLengthInstruction = `Minimum length of the password is <code>6</code>.`,
+  MaxLengthInstruction = `Maximum length of the password is <code>16</code>.`,
+  PatternInstruction = `
+    Characters Allowed:
+    Case-insensitive,
+    Numbers,
+    Special (
+      <code>!</code>,
+      <code>#</code>,
+      <code>$</code>,
+      <code>+</code>,
+      <code>-</code>,
+      <code>@</code>,
+      <code>^</code>,
+      <code>_</code>,
+      <code>~</code>
+      only
+    )`,
   PasswordsMismatchValidationFeedback = 'Passwords do not match.',
 }
 
--- src/app/web/home/shared/components/change-password/change-password.enum.ts
+++ src/app/web/home/shared/components/change-password/change-password.enum.ts
@@ -1,14 +1,29 @@
 export enum PASSWORD_FORM_CONTROL_LABELS {
   MinLength = 6,
   MaxLength = 16,
-  Pattern = '^[A-Za-z0-9!#$^*_<>]+$',
+  Pattern = '^[A-Za-z0-9!#$+-@^_~]+$',
   RequiredValidationFeedback = 'Field is required.',
   MinLengthValidationFeedback = 'Minimum length of the field is 6.',
   MaxLengthValidationFeedback = 'Maximum length of the field is 16.',
   PatternValidationFeedback = 'Invalid pattern.',
-  MinLengthInstruction = 'Minimum length of the password is 6.',
-  MaxLengthInstruction = 'Maximum length of the password is 16.',
-  PatternInstruction = 'Characters Allowed: Case-insensitive, Numbers, Special (!, #, $, ^, *, _, <, > only)',
+  MinLengthInstruction = `Minimum length of the password is <code>6</code>.`,
+  MaxLengthInstruction = `Maximum length of the password is <code>16</code>.`,
+  PatternInstruction = `
+    Characters Allowed:
+    Case-insensitive,
+    Numbers,
+    Special (
+      <code>!</code>,
+      <code>#</code>,
+      <code>$</code>,
+      <code>+</code>,
+      <code>-</code>,
+      <code>@</code>,
+      <code>^</code>,
+      <code>_</code>,
+      <code>~</code>
+      only
+    )`,
   PasswordsMismatchValidationFeedback = 'Passwords do not match.',
 }
 
